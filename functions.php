a0efb3858f94528024ccb02f939ab2e882abef64
Douglas Leal <douglas.leal@live.com>
2023-03-28 19:47:11 -0300

    usando bootstrap

--- functions.php
+++ functions.php
@@ -4,3 +4,6 @@ function exemplo_recursos_tema(){
 	add_theme_support( 'post-thumbnails' );
 }
 add_action('after_setup_theme', 'exemplo_recursos_tema');
+
+function exemplo_tamanho_excerpt() { return 50; }
+add_filter("excerpt_length", "exemplo_tamanho_excerpt");
--- index.php
+++ index.php
@@ -6,28 +6,46 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 </head>
 <body>
 <header>
     <h2>Header</h2>
     <hr>
 </header>
+<div class="container">
+    <h1 class="text-center">Hello</h1>
 
-<h1>Hello</h1>
+    <div class="row g-3">
+		<?php
+		if ( have_posts() ):
+			while ( have_posts() ):
+				the_post();
+				?>
+                <div class="col-6">
+                    <div class="card">
+                        <img style="height: 400px" src="<?= get_the_post_thumbnail_url() ?>" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php the_title() ?></h5>
+                            <p class="card-text"><?php the_excerpt(); ?></p>
+                        </div>
+                    </div>
+                </div>
+			<?php
 
-<?php
-if ( have_posts() ):
-	while ( have_posts() ):
-		the_post();
-		the_post_thumbnail();
-		the_content();
-	endwhile;
-endif;
-?>
-
+			endwhile;
+		endif;
+		?>
+    </div>
+</div>
 <footer>
     <hr>
     <h2>Footer</h2>
 </footer>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 </body>
 </html>
